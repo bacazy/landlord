8060d949f9cad70391626aff8acb97433bfa0d23
mi.zhangcheng <gc_zc@live.com>
2018-08-20 22:18:10 +0800

    init

---
+++ landlord-server/src/main/java/com/bacazy/landlord/LandlordApplication.java
@@ -0,0 +1,12 @@
+package com.bacazy.landlord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlordApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(LandlordApplication.class);
+    }
+}
